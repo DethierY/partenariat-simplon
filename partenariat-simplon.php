089feecac6a94bb84dcc7be484399ae74e52b707
DethierY <https://github.com/DethierY/Algo.git>
2017-12-07 17:33:44 +0100

    ajout plugin

--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,10 +1,26 @@
 <?php
+
 /*
+
+Plugin Name: Simplon
+
+Plugin URI: http://zero-plugin.com
+
+Description: Un plugin permettant d'identifier Simplon comme partenaire de Eïleen McIntosh
+
+Version: 0.1
+
+Author: Asgeir
+
+Author URI: http://Asgeir.com
+
+License: GPL2
 
 */
 
-
-
-
-
-?>
+function HelloWorldShortcode() {
+	return '<p>La publication de cet article est possible grâce à mon super partenaire <a href="https://simplon.co/">simplon.co</a> - une entreprise de
+l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+des formations GRATUITES pour devenir développeur web.</p>';
+}
+add_shortcode('Simplon', 'HelloWorldShortcode');
